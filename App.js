bea4f70cbbbce4482d19588732598d4d74cc8d37
shafreena <shafreena2000@gmail.com>
2024-02-06 08:41:14 +0530

    EP:1 Inception

---
+++ App.js
@@ -0,0 +1,5 @@
+const heading1 = React.createElement('h1',{id:'react'},'Hello World from React1');
+const heading2 = React.createElement('h1',{id:'react'},'Hello World from React2');
+const container = React.createElement('div',{},[heading1,heading2]);
+const root = ReactDOM.createRoot(document.getElementById('react-root'));
+root.render(container);
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # namaste-react
+
+EP -1 : Inception -> 1. Hello World from HTML 2. Hello World from Js 3. Hello World from React
