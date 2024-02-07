ecf98c225abfc2a0a6cf163c247aaf3f1aa0d8ab
shafreena <shafreena2000@gmail.com>
2024-02-07 15:39:34 +0530

    EP:2 Igniting the App

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const heading1 = React.createElement('h1',{id:'react'},'Hello World from React1');
 const heading2 = React.createElement('h1',{id:'react'},'Hello World from React2');
 const container = React.createElement('div',{},[heading1,heading2]);
